fd1e4cd0ff3b2e112fe6a0476af86caa65f7664b
Amanda Gama <amgama17@gmail.com>
2021-12-28 11:05:06 -0300

    Using JSX logic and ESLint improvements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import Pet from './Pet'
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
-    React.createElement(Pet, { name: 'Mike', animal: 'Dog', breed: 'Maltes' }),
-    React.createElement(Pet, { name: 'Misty', animal: 'Cat', breed: 'MultiColor' }),
-    React.createElement(Pet, { name: 'Scar', animal: 'Cat', breed: 'Angora' }),
-  ])
+  return(
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Mike" animal="Dog" breed="Maltes" />
+      <Pet name="Pandora" animal="Cat" breed="Angora" />
+      <Pet name="Scar" animal="Cat" breed="Maltes" />
+    </div>
+  )
 }
 
-ReactDOM.render(React.createElement(App, {}, null), document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
